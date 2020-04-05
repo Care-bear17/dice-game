4277c9397ec3ce14cf09fee09df46fa13772fd39
lukevaka <lukevakasisikakala@gmail.com>
2020-04-05 17:49:19 -0400

    Added style to roll dice button
    Imported material UI stylizing

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './Buttons.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-function buttons() {
-  return (
-    <div>
-     <button>Roll Dice</button>
-    </div>
-  );
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #039479, 30%, #004c82 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px"
+  }
+});
+
+export default function Hook() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Roll Dice</Button>;
 }
-
-export default buttons;
